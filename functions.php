a79f5e7ea4773d09d2a6071d68cd43221fc41d32
Jure Cotic <jc@lost.si>
2013-07-05 22:47:19 +0300

    functions cleanup, only loading maps js on front-page.php

--- functions.php
+++ functions.php
@@ -1,149 +1,164 @@
 <?php
 
-	//menus
-    function register_theme_menus() {
-      register_nav_menus(
-          array( 'main-menu' => __( 'Main Menu' ) )
-      );
-    }
-    add_action( 'init', 'register_theme_menus' );
-	
-	//include the main class file
+//menus
+function register_theme_menus()
+{
+    register_nav_menus(array(
+        'main-menu' => __('Main Menu')
+    ));
+}
+add_action('init', 'register_theme_menus');
+
+//include the main class file
 require_once("Tax-meta-class/Tax-meta-class.php");
-	
-	//custom taxonomy field
-	require_once("functions/custom_tax_fields.php");
-	
-	//scripts
-	
+
+//custom taxonomy field
+require_once("functions/custom_tax_fields.php");
+
+//scripts
+
 function wpbootstrap_scripts_with_jquery()
 {
+    
+    // Register the script like this for a theme:
+    wp_register_script('bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array(
+        'jquery'
+    ));
+        
+    if (is_front_page()) {
+		//currently gmaps is only needed on front-page
+		wp_register_script('gmaps-script', 'http://maps.google.com/maps/api/js?sensor=true', array(
+        'jquery'
+		));
 		
-	// Register the script like this for a theme:
-	wp_register_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ) );
-	wp_register_script( 'gmaps-script', 'http://maps.google.com/maps/api/js?sensor=true', array( 'jquery' ) );
-	wp_register_script( 'front-page-script', get_template_directory_uri() . '/js/front-page.js', array( 'jquery' ) );
-	
-	
-	// Settings for dynamic js
-	$settings = array(
-            'url' => esc_url( home_url( ' / ' ) )
+        wp_register_script('front-page-script', get_template_directory_uri() . '/js/front-page.js', array(
+            'jquery'
+        ));
+        // Settings for dynamic js
+        $settings = array(
+            'url' => esc_url(home_url(' / '))
         );
-	
-	wp_localize_script('front-page-script', 'settings', $settings);
-	
-	// For either a plugin or a theme, you can then enqueue the script:
-	wp_enqueue_script( 'bootstrap-script' );
-	wp_enqueue_script( 'gmaps-script' );
-	wp_enqueue_script( 'front-page-script' );
+        
+        wp_localize_script('front-page-script', 'settings', $settings);
+		wp_enqueue_script('gmaps-script');
+        wp_enqueue_script('front-page-script');
+    }
+        
+    // For either a plugin or a theme, you can then enqueue the script:
+    wp_enqueue_script('bootstrap-script');
+    
+    
 }
-add_action( 'wp_enqueue_scripts', 'wpbootstrap_scripts_with_jquery' );
+add_action('wp_enqueue_scripts', 'wpbootstrap_scripts_with_jquery');
 
 
-	//sidebar
-if ( function_exists('register_sidebar') )
-	register_sidebar(array(
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-	));
+//sidebar
+if (function_exists('register_sidebar'))
+    register_sidebar(array(
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
 
 /* Disable WordPress Admin Bar for all users but admins. */
-  show_admin_bar(false);	
-  //CUSTOM BOXES
-add_action( 'init', 'create_post_type' );
-function create_post_type() {
-	register_post_type( 'spots',
-		array(
-			'labels' => array(
-				'name' => __( 'Spoti' ),
-				'singular_name' => __( 'Spot' )
-			),
-		'taxonomies' => array('category'),
-		'supports' => array( 'title','editor', 'thumbnail' ),
-		'public' => true,
-		'has_archive' => true,
-		)
-	);
+show_admin_bar(false);
+//CUSTOM BOXES
+add_action('init', 'create_post_type');
+function create_post_type()
+{
+    register_post_type('spots', array(
+        'labels' => array(
+            'name' => __('Spoti'),
+            'singular_name' => __('Spot')
+        ),
+        'taxonomies' => array(
+            'category'
+        ),
+        'supports' => array(
+            'title',
+            'editor',
+            'thumbnail'
+        ),
+        'public' => true,
+        'has_archive' => true
+    ));
 }
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support('post-thumbnails');
 add_post_type_support('spots', 'excerpt');
-  /* Define the custom box */
-  
-add_action( 'add_meta_boxes', 'myplugin_add_custom_box' );
+/* Define the custom box */
+
+add_action('add_meta_boxes', 'myplugin_add_custom_box');
 
 // backwards compatible (before WP 3.0)
 // add_action( 'admin_init', 'myplugin_add_custom_box', 1 );
 
 /* Do something with the data entered */
-add_action( 'save_post', 'myplugin_save_postdata' );
+add_action('save_post', 'myplugin_save_postdata');
 
 /* Adds a box to the main column on the Post and Page edit screens */
-function myplugin_add_custom_box() {
-    $screens = array( 'spots' );
+function myplugin_add_custom_box()
+{
+    $screens = array(
+        'spots'
+    );
     foreach ($screens as $screen) {
-        add_meta_box(
-            'coordinates',
-            __( 'Coordinates', 'myplugin_textdomain' ),
-            'myplugin_inner_custom_box',
-            $screen
-        );
+        add_meta_box('coordinates', __('Coordinates', 'myplugin_textdomain'), 'myplugin_inner_custom_box', $screen);
     }
 }
 
 /* Prints the box content */
-function myplugin_inner_custom_box( $post ) {
-
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'myplugin_noncename' );
-
-  // The actual fields for data entry
-  // Use get_post_meta to retrieve an existing value from the database and use the value for the form
-  $lat = get_post_meta( $post->ID, 'lat', true );
-  echo '<label for="lat">';
-       _e("Lat", 'myplugin_textdomain' );
-  echo '</label> ';
-  echo '<input type="text" id="lat" name="lat" value="'.esc_attr($lat).'" size="25" />';
-  
-  $lng = get_post_meta( $post->ID, 'lng', true );
-  echo '<label for="lng">';
-       _e("Lng", 'myplugin_textdomain' );
-  echo '</label> ';
-  echo '<input type="text" id="lng" name="lng" value="'.esc_attr($lng).'" size="25" />';
+function myplugin_inner_custom_box($post)
+{
+    
+    // Use nonce for verification
+    wp_nonce_field(plugin_basename(__FILE__), 'myplugin_noncename');
+    
+    // The actual fields for data entry
+    // Use get_post_meta to retrieve an existing value from the database and use the value for the form
+    $lat = get_post_meta($post->ID, 'lat', true);
+    echo '<label for="lat">';
+    _e("Lat", 'myplugin_textdomain');
+    echo '</label> ';
+    echo '<input type="text" id="lat" name="lat" value="' . esc_attr($lat) . '" size="25" />';
+    
+    $lng = get_post_meta($post->ID, 'lng', true);
+    echo '<label for="lng">';
+    _e("Lng", 'myplugin_textdomain');
+    echo '</label> ';
+    echo '<input type="text" id="lng" name="lng" value="' . esc_attr($lng) . '" size="25" />';
 }
 
 /* When the post is saved, saves our custom data */
-function myplugin_save_postdata( $post_id ) {
-
-  // First we need to check if the current user is authorised to do this action. 
-  if ( 'page' == $_POST['post_type'] ) {
-    if ( ! current_user_can( 'edit_page', $post_id ) )
+function myplugin_save_postdata($post_id)
+{
+    
+    // First we need to check if the current user is authorised to do this action. 
+    if ('page' == $_POST['post_type']) {
+        if (!current_user_can('edit_page', $post_id))
+            return;
+    } else {
+        if (!current_user_can('edit_post', $post_id))
+            return;
+    }
+    
+    // Secondly we need to check if the user intended to change this value.
+    if (!isset($_POST['myplugin_noncename']) || !wp_verify_nonce($_POST['myplugin_noncename'], plugin_basename(__FILE__)))
         return;
-  } else {
-    if ( ! current_user_can( 'edit_post', $post_id ) )
-        return;
-  }
-
-  // Secondly we need to check if the user intended to change this value.
-  if ( ! isset( $_POST['myplugin_noncename'] ) || ! wp_verify_nonce( $_POST['myplugin_noncename'], plugin_basename( __FILE__ ) ) )
-      return;
-
-  // Thirdly we can save the value to the database
-
-  //if saving in a custom table, get post_ID
-  $post_ID = $_POST['post_ID'];
-  //sanitize user input
-  $datalat = sanitize_text_field( $_POST['lat'] );
-  $datalng = sanitize_text_field( $_POST['lng'] );
-
-  // Do something with $mydata 
-  // either using 
-  add_post_meta($post_ID, 'lat', $datalat, true) or
-    update_post_meta($post_ID, 'lat', $datalat);
- add_post_meta($post_ID, 'lng', $datalng, true) or
-    update_post_meta($post_ID, 'lng', $datalng);
-  
+    
+    // Thirdly we can save the value to the database
+    
+    //if saving in a custom table, get post_ID
+    $post_ID = $_POST['post_ID'];
+    //sanitize user input
+    $datalat = sanitize_text_field($_POST['lat']);
+    $datalng = sanitize_text_field($_POST['lng']);
+    
+    // Do something with $mydata 
+    // either using 
+    add_post_meta($post_ID, 'lat', $datalat, true) or update_post_meta($post_ID, 'lat', $datalat);
+    add_post_meta($post_ID, 'lng', $datalng, true) or update_post_meta($post_ID, 'lng', $datalng);
+    
 }
 
 require_once('functions/updater.php');
--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<div class="row">
-  <div class="span8">
+<div class="row-fluid">
+  <div class="span7 offset1">
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<h1><?php the_title(); ?></h1>
